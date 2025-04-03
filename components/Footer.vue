ab202cecd4a555f0771b5a9679664800af1b2977
Loukavdt <loukavdt2002@gmail.com>
2025-04-03 18:02:06 +0200

    footer text en kleur aanpassing

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
  <!-- Footer -->
- <footer class="bg-gray-900 text-white text-center py-4 mt-12">
-        <p>Rue des Palais 70, 1030 Brussels | Site: Lukas@gmail.com</p>
+ <footer class="bg-red-600 text-white text-center py-4 mt-12">
+        <p>Rue des Palais 70, 1030 Brussels | Sint-Lukas@gmail.com</p>
         <div class="mt-2 text-sm">
           <NuxtLink to="/Privacy policy" class="underline">Privacy policy</NuxtLink> |
           <NuxtLink to="/cookies" class="underline">Cookies policy</NuxtLink>
