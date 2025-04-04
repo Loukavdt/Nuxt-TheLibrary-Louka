fda45a603fe97c0ac4c6ecf5bb21bd13446c4d1c
Loukavdt <loukavdt2002@gmail.com>
2025-04-04 09:49:45 +0200

    cookies en privacy page gekoppeld

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,7 +4,7 @@
  <footer class="bg-red-600 text-white text-center py-5 mt-12">
         <p>Rue des Palais 70, 1030 Brussels | Sint-Lukas@gmail.com</p>
         <div class="mt-2 text-sm">
-          <NuxtLink to="/Privacy policy" >Privacy policy</NuxtLink> |
+          <NuxtLink to="/privacypolicy" >Privacy policy</NuxtLink> |
           <NuxtLink to="/cookies" >Cookies policy</NuxtLink>
         </div>
       </footer>
